48e1ae710eaa4e38b6d221e9a7bd2df14fe4e4a1
Lee <94841127+galaxian@users.noreply.github.com>
2024-05-28 12:23:13 +0900

    test: 제한 인원수를 초과하여 승인할 경우 예외 발생

--- src/test/java/clofi/runningplanet/crew/service/CrewServiceTest.java
+++ src/test/java/clofi/runningplanet/crew/service/CrewServiceTest.java
@@ -769,4 +769,45 @@ class CrewServiceTest {
 		assertThatThrownBy(() -> crewService.proceedApplyCrew(reqDto, crewId, memberId))
 			.isInstanceOf(NotFoundException.class);
 	}
+
+	@DisplayName("제한 인원수를 초과하여 승인할 경우 예외 발생")
+	@Test
+	void failApproveCrewByOverLimitMember() {
+		//given
+		ProceedApplyReqDto reqDto = new ProceedApplyReqDto(2L, true);
+		Long crewId = 1L;
+		Long memberId = 1L;
+
+		Crew crew = new Crew(1L, 1L, "구름 크루", 10, 50,
+			RUNNING, MANUAL, "구름 크루는 성실한 크루", 5, 100,
+			0, 0);
+		Member member1 = Member.builder()
+			.id(2L)
+			.nickname("닉네임1")
+			.age(30)
+			.gender(Gender.MALE)
+			.profileImg("https://image-url1.com")
+			.avgDistance(50)
+			.totalDistance(2000)
+			.runScore(80)
+			.build();
+
+		CrewMember crewMember = new CrewMember(1L, crew, MEMBER, Role.LEADER);
+		CrewApplication crewApplication = new CrewApplication(1L, "크루 신청글", Approval.PENDING, crew, member1);
+
+		given(crewRepository.findById(anyLong()))
+			.willReturn(Optional.of(crew));
+		given(crewMemberRepository.findByMemberId(anyLong()))
+			.willReturn(Optional.of(crewMember))
+			.willReturn(Optional.empty());
+		given(crewApplicationRepository.findByCrewIdAndMemberId(anyLong(), anyLong()))
+			.willReturn(Optional.of(crewApplication));
+		given(crewMemberRepository.countByCrewId(anyLong()))
+			.willReturn(10);
+
+		//when
+		//then
+		assertThatThrownBy(() -> crewService.proceedApplyCrew(reqDto, crewId, memberId))
+			.isInstanceOf(ConflictException.class);
+	}
 }
